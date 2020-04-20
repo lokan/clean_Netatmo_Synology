b374abdbedf531fc81fc261127c322ee02d758c6
LoKan Sardari <lokansardari@gmail.com>
2020-04-20 20:53:49 +0200

    Create clean_security.sh

---
+++ clean_security.sh
@@ -0,0 +1,3 @@
+rm -fr /volume3/Security/NetatmoSecrete1/`date -d "10 month ago" +%Y`/`date -d "10 month ago" +%m`/
+rm -fr /volume3/Security/NetatmoSecrete2/`date -d "10 month ago" +%Y`/`date -d "10 month ago" +%m`/
+rm -fr /volume3/Security/NetatmoSalon/`date -d "10 month ago" +%Y`/`date -d "10 month ago" +%m`/
