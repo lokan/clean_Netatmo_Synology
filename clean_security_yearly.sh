17c95abc040cdfcc6d68eb7834fbd721fb61b022
LoKan Sardari <lokansardari@gmail.com>
2020-04-20 20:53:26 +0200

    Create clean_security_yearly.sh

---
+++ clean_security_yearly.sh
@@ -0,0 +1,3 @@
+rm -fr /volume3/Security/NetatmoSecrete1/`date -d "1 year ago" +%Y`/
+rm -fr /volume3/Security/NetatmoSecrete2/`date -d "1 year ago" +%Y`/
+rm -fr /volume3/Security/NetatmoSalon/`date -d "1 year ago" +%Y`/
