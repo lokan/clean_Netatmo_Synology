64ab04301adc43384aa4abf9351eed6134acb858
Quentin B <bihetq@gmail.com>
2020-04-21 14:31:09 +0200

    Just improve the scripts

--- clean_security_yearly.sh
+++ clean_security_yearly.sh
@@ -1,3 +1,1 @@
-rm -fr /volume3/Security/NetatmoSecrete1/`date -d "1 year ago" +%Y`/
-rm -fr /volume3/Security/NetatmoSecrete2/`date -d "1 year ago" +%Y`/
-rm -fr /volume3/Security/NetatmoSalon/`date -d "1 year ago" +%Y`/
+rm -fr /volume3/Security/{NetatmoSecrete1,NetatmoSecrete2,NetatmoSalon}/`date -d "1 year ago" +%Y`/
